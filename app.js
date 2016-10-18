7ba418e883fcb06d2bbe447c57c5ae5c95bf3e6a
Hessel Dijkstra <hessel.dijkstra@ing.nl>
2016-10-18 16:47:47 +0200

    Small refactor

--- app.js
+++ app.js
@@ -3,13 +3,13 @@ var rxApp = angular.module('rxApp', []);
 rxApp.controller('rxController', function rxController($scope, $anchorScroll, $location) {
     $scope.users = [];
 
-    $scope.requestStream = Rx.Observable.interval(3000)
+    var requestStream = Rx.Observable.interval(3000)
         .map(function () {
             var randomOffset = Math.floor(Math.random() * 500);
             return 'https://api.github.com/users?since=' + randomOffset + 'page=1&per_page=1&client_id=86ec4cc09f775228c518&client_secret=9ee0e82b81139a6c5f97c610d7981cf11967c3c4';
         });
 
-    $scope.source = $scope.requestStream
+    $scope.source = requestStream
         .flatMap(function (requestUrl) {
             return Rx.Observable.fromPromise($.getJSON(requestUrl));
         });
