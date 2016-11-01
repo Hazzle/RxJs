5db54c0cfcf7479e29aeeda1426f33196770e060
Hessel Dijkstra <hessel.dijkstra@ing.nl>
2016-11-01 13:29:47 +0100

    Add merge

--- app.js
+++ app.js
@@ -3,13 +3,21 @@ var rxApp = angular.module('rxApp', []);
 rxApp.controller('rxController', function rxController($scope, $anchorScroll, $location) {
     $scope.users = [];
 
-    var requestStream = Rx.Observable.interval(3000)
+    var requestStream1 = Rx.Observable.interval(3000)
         .map(function () {
             var randomOffset = Math.floor(Math.random() * 500);
             return 'https://api.github.com/users?since=' + randomOffset + 'page=1&per_page=1&client_id=86ec4cc09f775228c518&client_secret=9ee0e82b81139a6c5f97c610d7981cf11967c3c4';
         });
 
-    var responseStream = requestStream
+    var requestStream2 = Rx.Observable.interval(3000)
+        .map(function () {
+            var randomOffset = Math.floor(Math.random() * 500);
+            return 'https://api.github.com/users?since=' + randomOffset + 'page=1&per_page=1&client_id=86ec4cc09f775228c518&client_secret=9ee0e82b81139a6c5f97c610d7981cf11967c3c4';
+        });
+
+    var mergeStream = Rx.Observable.merge(requestStream1, requestStream2);
+
+    var responseStream = mergeStream
         .flatMap(function (requestUrl) {
             return Rx.Observable.fromPromise($.getJSON(requestUrl));
         });
